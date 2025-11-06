9842b51dfc5d3f4a530eb34c2a90f9137acb80bc
anubhavsingh2027 <anubhavsingh2027@gmail.com>
2025-11-06 15:54:52 +0530

    update

--- api.js
+++ api.js
@@ -57,4 +57,9 @@ app.post('/sendMail', async (req, res) => {
 });
 
 // ✅ Start server
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
